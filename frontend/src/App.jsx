6e67a28a1c7090f9c5fecd8f7bc8da5caf6da101
Anujahirwar01 <anujahirwar77010@gmail.com>
2025-05-12 22:26:49 +0530

    front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AppRoutes from "./routes/AppRoutes.jsx";
-
+import { UserProvider } from "./context/user.context.jsx";
 
 const App = () => {
   return (
-    <AppRoutes/>
+    <UserProvider>
+        <AppRoutes />
+    </UserProvider>
   );
 }
 export default App;
--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React,{useContext} from "react";
+import { Routes} from "react-router-dom";
+import { UserContext } from "../context/user.context";
 
 const Home = () => {
+    const { user } = useContext(UserContext)
     return (
-        <div>
-            home
-        </div>
+        
     )
 }
 export default Home;
