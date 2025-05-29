8ea0bdc762fbb43407844035341977846ba0e4d1
Anujahirwar01 <anujahirwar77010@gmail.com>
2025-05-29 22:42:17 +0530

    baxk

--- backend/routes/project.routes.js
+++ backend/routes/project.routes.js
@@ -24,4 +24,9 @@ router.post('/create',
    projectController.addUserToProject
  )
 
+ router.get('/get-project/:projectId',
+    authMiddleware.authUser,
+    projectController.getProjectById
+)
+
 export default router;
--- backend/services/project.service.js
+++ backend/services/project.service.js
@@ -80,3 +80,16 @@ export const addUserToProject = async ({projectId, users,userId}) => {
     return updatedProject;
 }
 
+export const getProjectById = async({ projectId }) => {
+    if(!projectId) {
+        throw new Error('Project ID is required');
+    }
+    if(!mongoose.Types.ObjectId.isValid(projectId)) {
+        throw new Error('Invalid Project ID');
+    }
+    const project = await projectModel.findOne({
+        _id: projectId
+    }).populate('users');
+    
+    return project;
+}
