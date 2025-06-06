54d793f7025f9cf4dd17294c6be593dbf3be173c
Anujahirwar01 <anujahirwar77010@gmail.com>
2025-06-06 21:36:38 +0530

    front

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -4,16 +4,16 @@ import Login from "../screens/login.jsx";
 import Register from "../screens/Register.jsx";
 import Home from "../screens/Home.jsx";
 import Project from "../screens/Project.jsx";
-// import UserAuth from "../components/UserAuth.jsx";
+import UserAuth from "../auth/UserAuth.jsx";
 
 const AppRoutes = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home/>}/>
+                <Route path="/" element={<UserAuth><Home/></UserAuth>}/>
                 <Route path="/login" element={<Login/>}/>
                 <Route path="/register" element={<Register/>}/>
-                <Route path="/project" element={<Project />} />
+                <Route path="/project" element={<UserAuth><Project /></UserAuth>} />
                 {/* <Route path="/about" element={<h1 className="text-3xl font-bold underline">About</h1>} />
                 <Route path="/contact" element={<h1 className="text-3xl font-bold underline">Contact</h1>} /> */}
             </Routes>
