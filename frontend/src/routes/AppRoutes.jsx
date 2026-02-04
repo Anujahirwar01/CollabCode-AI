e6ae7c1762b940166b6a0538ee019ae1afaa95c1
Anujahirwar01 <anujahirwar77010@gmail.com>
2026-02-04 17:53:23 +0530

    fix routes

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import { Route, BrowserRouter, Routes } from 'react-router-dom'
-import Login from '../screens/login.jsx'
-import Register from '../screens/Register'
-import Home from '../screens/Home'
-import Project from '../screens/Project'
-import UserAuth from '../auth/UserAuth'
-
+import React from 'react';
+import { Route, BrowserRouter, Routes, Navigate } from 'react-router-dom'; // <-- Add Navigate here
+import Login from '../screens/login.jsx';
+import Register from '../screens/Register';
+import Home from '../screens/Home';
+import Project from '../screens/Project';
+import Profile from '../screens/Profile';
+import UserAuth from '../auth/UserAuth';
 const AppRoutes = () => {
     return (
         <BrowserRouter>
 
             <Routes>
                 <Route path="/" element={<UserAuth><Home /></UserAuth>} />
-                
+                <Route path="/home" element={<Navigate to="/" replace />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
+                <Route path="/profile" element={<UserAuth><Profile /></UserAuth>} />
                 {/* ✅ CORRECTED: Added the dynamic :projectId parameter to the path */}
                 <Route path="/project/:projectId" element={<UserAuth><Project /></UserAuth>} />
             </Routes>
